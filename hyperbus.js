b18c91356a3c93d63c94e448f4b6128d67b7e153
Jonathan Dupré <hello@jonathandupre.com>
2018-08-12 20:30:10 -0400

    create feed only -after- piping streams w/ gateway

--- hyperbus.js
+++ hyperbus.js
@@ -23,19 +23,14 @@ function hyperbus (state, emitter) {
   var log = multifeed(hypercore, storage, opts)
 
   emitter.on('DOMContentLoaded', () => {
-    log.writer('local', onWriter)
-  })
-
-  function onWriter (err, feed, id) {
-    if (err) throw err
-
     var key = state.query.key || crypto.randomBytes(32).toString('hex')
     var remote = websocket(`${PROTOCOL}://${GATEWAY}/feeds/${key}`)
     var local = log.replicate({ encrypt: false, live: true })
 
     log.on('feed', (peerFeed, name) => {
+      console.log('on:feed', name)
       var peerFeedStream = peerFeed.createReadStream({ live: true })
-      var bus = new Bus(feed, peerFeedStream)
+      var bus = new Bus(log.feeds()[0], peerFeedStream)
 
       emitter.emit('hyperbus:ready', bus)
     })
@@ -43,5 +38,11 @@ function hyperbus (state, emitter) {
     pump(remote, local, remote, err => {
       if (err) throw err
     })
+
+    log.writer('local', onWriter)
+  })
+
+  function onWriter (err, feed, id) {
+    if (err) throw err
   }
 }
